d2e7ef96259053729a1f6ea4b55800eded41467f
Florian Hines <syn@ronin.io>
2022-10-20 13:15:11 -0500

    feat: edk based dbt extension (#2)
    Co-authored-by: Pat Nadolny <patnadolny@gmail.com>

--- dbt_ext/extension.py
+++ dbt_ext/extension.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 import os
-import pkgutil
+import shutil
 import subprocess
 import sys
 from pathlib import Path
@@ -13,16 +13,76 @@ from meltano.edk import models
 from meltano.edk.extension import ExtensionBase
 from meltano.edk.process import Invoker, log_subprocess_error
 
+try:
+    from importlib.resources import files as ir_files
+except ImportError:
+    from importlib_resources import files as ir_files
+
 log = structlog.get_logger()
+
+
+class MissingProfileTypeError(Exception):
+    """Missing profile type error."""
+
+    pass
 
 
 class dbt(ExtensionBase):
     """Extension implementing the ExtensionBase interface."""
 
     def __init__(self) -> None:
-        """Initialize the extension."""
-        self.dbt_bin = "dbt"  # verify this is the correct name
-        self.dbt_invoker = Invoker(self.dbt_bin)
+        """Initialize the extension.
+
+        Raises:
+            MissingProfileTypeError: If the profile type is not set.
+        """
+        self.dbt_bin = "dbt"
+        self.dbt_ext_type = os.getenv("DBT_EXT_TYPE", None)
+        if not self.dbt_ext_type:
+            raise MissingProfileTypeError("DBT_EXT_TYPE must be set")
+        self.dbt_project_dir = Path(os.getenv("DBT_EXT_PROJECT_DIR", "transform"))
+        self.dbt_profiles_dir = Path(
+            os.getenv("DBT_EXT_PROFILES_DIR", self.dbt_project_dir / "transform")
+        )
+        self.dbt_invoker = Invoker(self.dbt_bin, cwd=self.dbt_project_dir)
+        self.skip_pre_invoke = (
+            os.getenv("DBT_EXT_SKIP_PRE_INVOKE", "false").lower() == "true"
+        )
+
+    def pre_invoke(self, invoke_name: str | None, *invoke_args: Any) -> None:
+        """Pre-invoke hook.
+
+        Runs `dbt deps` to ensure dependencies are up-to-date on every invocation.
+
+        Args:
+            invoke_name: The name of the command that will eventually be invoked.
+            invoke_args: The arguments that will be passed to the command.
+        """
+        if self.skip_pre_invoke:
+            log.debug("skipping pre-invoke as DBT_EXT_SKIP_PRE_INVOKE is set")
+            return
+
+        if invoke_name in ["deps", "clean"]:
+            log.debug("skipping pre-invoke as command being invoked is deps or clean")
+            return
+
+        try:
+            log.info("Extension executing `dbt clean`...")
+            self.dbt_invoker.run_and_log("clean")
+        except subprocess.CalledProcessError as err:
+            log_subprocess_error(
+                "dbt clean", err, "pre invoke step of `dbt clean` failed"
+            )
+            sys.exit(err.returncode)
+
+        try:
+            log.info("Extension executing `dbt deps`...")
+            self.dbt_invoker.run_and_log("deps")
+        except subprocess.CalledProcessError as err:
+            log_subprocess_error(
+                "dbt deps", err, "pre invoke step of `dbt deps` failed"
+            )
+            sys.exit(err.returncode)
 
     def invoke(self, command_name: str | None, *command_args: Any) -> None:
         """Invoke the underlying cli, that is being wrapped by this extension.
@@ -32,6 +92,10 @@ class dbt(ExtensionBase):
             command_args: The arguments to pass to the command.
         """
         try:
+            command_msg = command_name if command_name else self.dbt_bin
+            if len(command_args) > 0:
+                command_msg += f" {command_args[0][0]}"
+            log.info(f"Extension executing `{command_msg}`...")
             self.dbt_invoker.run_and_log(command_name, *command_args)
         except subprocess.CalledProcessError as err:
             log_subprocess_error(f"dbt {command_name}", err, "dbt invocation failed")
@@ -53,4 +117,48 @@ class dbt(ExtensionBase):
                     name="dbt_invoker", description="pass through invoker"
                 ),
             ]
+        )
+
+    def initialize(self, force: bool = False) -> None:
+        """Initialize the extension.
+
+        Args:
+            force: Whether to force initialization.
+        """
+        if not self.dbt_project_dir.exists():
+            log.info("creating dbt project directory", path=self.dbt_project_dir)
+            self.dbt_project_dir.mkdir(parents=True, exist_ok=True)
+
+        for entry in ir_files("files_dbt_ext.bundle.transform").iterdir():
+            if entry.name == "__pycache__":
+                continue
+            log.debug(f"deploying {entry.name}", entry=entry, dst=self.dbt_project_dir)
+            if entry.is_file():
+                shutil.copy(entry, self.dbt_project_dir / entry.name)
+            else:
+                shutil.copytree(
+                    entry, self.dbt_project_dir / entry.name, dirs_exist_ok=True
+                )
+
+        if not self.dbt_profiles_dir.exists():
+            log.info("creating dbt profiles directory", path=self.dbt_profiles_dir)
+            self.dbt_profiles_dir.mkdir(parents=True, exist_ok=True)
+
+        for entry in ir_files("files_dbt_ext.profiles").iterdir():
+            if entry.name == self.dbt_ext_type and entry.is_dir():
+                log.debug(
+                    f"deploying {entry.name} profile",
+                    entry=entry,
+                    dst=self.dbt_profiles_dir,
+                )
+                shutil.copytree(entry, self.dbt_profiles_dir, dirs_exist_ok=True)
+                break
+        else:
+            log.error(f"dbt type {self.dbt_ext_type} had no matching profile.")
+
+        log.info(
+            "dbt initialized",
+            dbt_ext_type=self.dbt_ext_type,
+            dbt_project_dir=self.dbt_project_dir,
+            dbt_profiles_dir=self.dbt_profiles_dir,
         )
--- tests/test_dbt_ext.py
+++ tests/test_dbt_ext.py
@@ -1,5 +1,40 @@
-from dbt_ext.main import app
+from unittest.mock import patch
 
 
-def test_app_name():
+def test_app_name(monkeypatch):
+    monkeypatch.setenv("DBT_EXT_TYPE", "test")
+    from dbt_ext.main import app
+
     assert app.info.name == "dbt"
+
+
+def test_pre_invoke(monkeypatch):
+    """Verify that pre-invokes are skipped for deps and clean."""
+    monkeypatch.setenv("DBT_EXT_TYPE", "test")
+    from dbt_ext.main import dbt
+
+    dbt_ext = dbt()
+
+    dbt_ext.skip_pre_invoke = True
+    with patch.object(dbt_ext.dbt_invoker, "run_and_log") as mock_run_and_log:
+        dbt_ext.pre_invoke("deps", [])
+        mock_run_and_log.assert_not_called()
+
+        dbt_ext.pre_invoke("clean", [])
+        mock_run_and_log.assert_not_called()
+
+        dbt_ext.pre_invoke("run", [])
+        mock_run_and_log.assert_not_called()
+
+    dbt_ext.skip_pre_invoke = False
+    with patch.object(dbt_ext.dbt_invoker, "run_and_log") as mock_run_and_log:
+        dbt_ext.pre_invoke("deps", [])
+        mock_run_and_log.assert_not_called()
+
+        dbt_ext.pre_invoke("clean", [])
+        mock_run_and_log.assert_not_called()
+
+        dbt_ext.pre_invoke("run", [])
+        assert mock_run_and_log.call_count == 2
+        assert mock_run_and_log.call_args_list[0][0][0] == "clean"
+        assert mock_run_and_log.call_args_list[1][0][0] == "deps"
